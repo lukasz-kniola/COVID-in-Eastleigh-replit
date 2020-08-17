bf08e24b22c562a14331658fb7701f4b70405772
Lukasz <lukasz.kniola@gmail.com>
2020-08-17 15:42:28 +0000

    changed days for colours from 10 to 8

--- main.py
+++ main.py
@@ -9,82 +9,93 @@ print("New data downloaded.")
 print("Last data update: " + source['metadata']['lastUpdatedAt'])
 
 # Subset
-subset = ["Eastleigh","Test Valley","Winchester","Fareham","Southampton","Portsmouth"]
+subset = [
+    "Eastleigh", "Test Valley", "Winchester", 
+    "Southampton", "Fareham", "Gosport", "Portsmouth",
+]
 
 # All days
 sdate = datetime.strptime("2020-01-01", '%Y-%m-%d')
-edate = datetime.strptime(source['metadata']['lastUpdatedAt'][0:10], '%Y-%m-%d')
+edate = datetime.strptime(source['metadata']['lastUpdatedAt'][0:10],
+                          '%Y-%m-%d')
 span = edate - sdate
-days = [(sdate + timedelta(days=i)).strftime("%Y-%m-%d") for i in range(span.days + 1)]
+days = [(sdate + timedelta(days=i)).strftime("%Y-%m-%d")
+        for i in range(span.days + 1)]
 
 # All areas
 areas = set([x["areaName"] for x in source["ltlas"] + source["utlas"]])
 
 # Table
-data = {day:{area:-1 for area in areas} for day in days}
+data = {day: {area: -1 for area in areas} for day in days}
 
 # Update with data
 for x in source["ltlas"] + source["utlas"]:
-    data[x["specimenDate"]][x["areaName"]] = x["totalLabConfirmedCases"]
+	data[x["specimenDate"]][x["areaName"]] = x["totalLabConfirmedCases"]
 
 #LOCF
-locf = {area:0 for area in areas}
+locf = {area: 0 for area in areas}
 
 for day in days:
-    for area in areas:
-        if data[day][area] == -1:
-            data[day][area] = locf[area]
-        else:
-            locf[area] = data[day][area]
+	for area in areas:
+		if data[day][area] == -1:
+			data[day][area] = locf[area]
+		else:
+			locf[area] = data[day][area]
+
 
 def offset(dtc, offset):
-    return (datetime.strptime(dtc, '%Y-%m-%d') + timedelta(days=offset)).isoformat()[:10]
+	return (datetime.strptime(dtc, '%Y-%m-%d') +
+	        timedelta(days=offset)).isoformat()[:10]
+
 
 def report(days):
-    print("\n\nDate".ljust(13), end='')
-    print(" ".join([i.center(11,' ') for i in subset]))
+	print("\n\nDate".ljust(13), end='')
+	print(" ".join([i.center(11, ' ') for i in subset]))
 
-    for _date in list(data.keys())[-days:]:
-        print(_date.ljust(11), end='')
-        for j in subset:
-            _cases = data[_date][j]
+	for _date in list(data.keys())[-days:]:
+		print(_date.ljust(11), end='')
+		for j in subset:
+			_cases = data[_date][j]
 
-            _cases_1 = data[offset(_date,-1)][j]
-            _cases_10 = data[offset(_date,-8)][j]
-            _diff1 = _cases - _cases_1
-            _diff10 = _cases - _cases_10
+			_cases_1 = data[offset(_date, -1)][j]
+			_cases_8 = data[offset(_date, -8)][j]
+			_diff1 = _cases - _cases_1
+			_diff8 = _cases - _cases_8
 
-            if _diff10 == 0:
-                _tx = "\33[94m"+str(_cases)+"\033[0m"
-            elif _diff10 <=1:
-                _tx = "\33[92m"+str(_cases)+"\033[0m"
-            elif _diff10 <=4:
-                _tx = "\33[93m"+str(_cases)+"\033[0m"
-            elif _diff10 <=9:
-                _tx = "\33[33m"+str(_cases)+"\033[0m"
-            else:
-                _tx = "\33[91m"+str(_cases)+"\033[0m"
+			if _diff8 == 0:
+				_tx = "\33[94m" + str(_cases) + "\033[0m"
+			elif _diff8 <= 2:
+				_tx = "\33[92m" + str(_cases) + "\033[0m"
+			elif _diff8 <= 5:
+				_tx = "\33[93m" + str(_cases) + "\033[0m"
+			elif _diff8 <= 9:
+				_tx = "\33[33m" + str(_cases) + "\033[0m"
+			else:
+				_tx = "\33[91m" + str(_cases) + "\033[0m"
 
-            if _diff1 <= 4:
-                _inc = "+"*_diff1 + " "*(4-_diff1)
-            else:
-                _inc = "+++>"
+			if _diff1 <= 4:
+				_inc = "+" * _diff1 + " " * (4 - _diff1)
+			else:
+				_inc = "+++>"
 
-            print(_tx.rjust(16) + _inc, end=' ')
+			print(_tx.rjust(16) + _inc, end=' ')
 
-        print()
+		print()
+
 
 report(15)
 while True:
-    show_days = input("\nHow many days back? ")
-    if show_days == "":
-        new_area = input("Any other area? ")
-        if new_area == "":
-            break
-        elif new_area in areas:
-            subset = [new_area,] + subset[0:-1]
-            show_days = 15
-        else:
-            print("\n" + new_area + " not recognized.")
-            show_days = 15
-    report(int(show_days))
+	show_days = input("\nHow many days back? ")
+	if show_days == "":
+		new_area = input("Any other area? ")
+		if new_area == "":
+			break
+		elif new_area in areas:
+			subset = [
+			    new_area,
+			] + subset[0:-1]
+			show_days = 15
+		else:
+			print("\n" + new_area + " not recognized.")
+			show_days = 15
+	report(int(show_days))
